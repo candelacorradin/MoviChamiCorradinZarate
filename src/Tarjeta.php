363a5ccb3dca64c2793659e16011ebe2277703ca
Aldana Zarate <31429603+alduzarate@users.noreply.github.com>
2017-10-08 14:46:13 -0300

    Update Tarjeta.php

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -3,8 +3,11 @@
 namespace TpFinal;
 
 class Tarjeta {
-
+protected $saldo;
     public function saldo() {
         return 0;
+    }
+    public function cargar($monto){
+    $this->saldo+=$monto;
     }
 }
