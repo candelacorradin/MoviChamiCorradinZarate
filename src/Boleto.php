2d8c53a5e8ac61d1a562c79f7352acbebeaa240c
sofiachami <sofiachami99@gmail.com>
2017-10-17 22:15:18 -0300

    objeto tarjeta y funciones

--- src/Boleto.php
+++ src/Boleto.php
@@ -1,52 +1,37 @@
 <?php
+<?php
 namespace TpFinal;
 class Boleto{
 	protected $fecha;
 	protected $hora;
-	protected $tipo;
+	protected $fechaant;
+	protected $horaant;
+	protected $tipoboleto;
 	protected $saldo;
 	protected $saldoAcumulado;
 	protected $linea;
 	protected $id_tarj;
-
-
-	public function __construct( int $id_tarjeta, int $saldo, $saldoac = 0 ){
+	public function __construct(Tarjeta $tar, $tipoboleto, $linea, $saldoac = 0 ){
 		$this->fecha = date('d-m-Y');
+		$this->tar->tipoboleto= $t;
 		$this->hora = date('H:m:s');
-		$this->saldoAcumulado = $saldoac;
-		$this->saldo = $saldo;
-		$this->id_tarj = $id_tarjeta;
-
+		$this->tar->saldoAcumulado = $saldoac;
+		$this->linea = $linea;
 	}
 
 	public getBoleto(){
-		print "FECHA: ". $this->fecha . "\nTIPO: ". $this->tipo . "\nLINEA DE COLECTIVO: ". $this->linea . "\nSALDO: ". $this->saldo . "\nID: ". $this->id_tarj;
+		print "FECHA: ". $this->fecha . "\nTIPO: ". $this->tar->tipoboleto. "\nLINEA DE COLECTIVO: ". $this->linea . "\nSALDO: ". $this->tar->saldo . "\nID: ". $this->tar->id_tarj;
 
 	}
-	public function Pasaje(){
-		if($this->tipo == "Normal"){
-			Normal();
-		}
-		if($this->tipo == "MedioBoleto"){
-			Medio();
-		}
-		else{
-			return "Tipo de pasaje invalido."
-		}
-		
-
-
-	}
-
-
+	
 	public function Normal(){
-		$p  = $this->saldo - $this->saldoAcumulado - 9.70;
+		$p  = $this->tar->saldo - $this->tar->saldoAcumulado - 9.70;
 		if($p<0){
 			ViajePlus();
 		}
 		else{
-			$this->saldo = $p;
-			$this->saldoAcumulado = 0;
+			$this->tar->saldo = $p;
+			$this->tar->saldoAcumulado = 0;
 		}
 
 		//AGREGAR CASO DE TRASBORDO y medio boleto
@@ -54,27 +39,22 @@ class Boleto{
 	}
 
 	public function ViajePlus(){
-		if($this->saldoAcumulado < (9.70*2)){
-			$this->saldoAcumulado= $this->saldoAcumulado - 9.70;
+		if($this->tar->saldoAcumulado < (9.70*2)){
+			$this->tar->saldoAcumulado= $this->tar->saldoAcumulado + 9.70;
 		}
 		else {
-			return "Ya han sido utilizados los dos (2) viajes plus. Recargue su tarjeta.";
-
-		}
-
-
+			return "Ya han sido utilizados los dos (2) viajes plus. Recargue su tarjeta.";}
 	}
 
 	public function Medio(){
-		$p  = $this->saldo - $this->saldoAcumulado - 4.35;
+		$p  = $this->tar->saldo - $this->tar->saldoAcumulado - 4.35;
 		if($p<0){
 			ViajePlus();
 		}
 		else{
-			$this->saldo = $p;
-			$this->saldoAcumulado = 0;
+			$this->tar->saldo = $p;
+			$this->tar->saldoAcumulado = 0;
 		}
 		
 	}
-
 }
