904e19894eb35dcb8df93924f27890800a5f64a7
candelacorradin <32111070+candelacorradin@users.noreply.github.com>
2017-10-30 17:02:23 -0300

    Update Tarjeta.php
    función viaje normal

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -4,9 +4,11 @@ include 'Boleto.php';
 
 class Tarjeta {
 protected $saldo;
+protected $saldoAcumulado;
 protected $id;
 protected $fechaanterior;
 protected $diaanterior;
+protected $tipo;
     public function __construct($id){
     $this->saldo=0;
     $this->id=$id;
@@ -33,23 +35,36 @@ protected $diaanterior;
   }
 
   public function Viaje($transporte, Boleto $b){  
-      if( is_a($transporte,'Colectivo') ){
-    if( $b->tipoboleto == "Normal" ){
-      $b->Normal();
-    }
-    if( $this->tipoboleto == "MedioBoleto" ){
-      $b->Medio();
-    }
-    else{
-      return "Tipo de viaje invalido."
-    }
+    if( is_a($transporte,'Colectivo') ){
+    //ACÁ VAN LOS IF PARA VER SI ES TRASBORDO PRIMERO, CONVIENE HACER UNA FUNCIÓN QUE LO DETERMINE
+        if( $this->tipo == "Normal" ){
+            $this->Normal();
+        }
+        if( $this->tipo == "MedioBoleto" ){
+            $this->Medio();
+        }
+        else {
+        return "Tipo de viaje invalido."
+        }
    }
       
     if(is_a($transporte,'Bicicleta') )
     {
-        $b->viajeBici();
+        $this->viajeBici();
     }
 
   }
+    public function Normal(){
+		$p  = $this->saldo - $this->saldoAcumulado - 9.70;
+		if($p<0){
+			$this->ViajePlus();
+		}
+		else{
+			$this-saldo = $p;
+			$this->saldoAcumulado = 0;
+		}
+		$this->fechaanterior=$this->fechatras;
+		$this->diaanterior=$this->diasemana;
+	}
 }
 ?>
