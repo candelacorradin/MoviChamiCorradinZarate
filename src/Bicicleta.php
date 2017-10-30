8024b93ddd865e87f29fe78d86fddb07ac91ca98
sofiachami <sofiachami99@gmail.com>
2017-10-29 22:33:50 -0300

    Update Bicicleta.php

--- src/Bicicleta.php
+++ src/Bicicleta.php
@@ -2,12 +2,16 @@
 namespace TpFinal;
 class Bicicleta {
   protected $EnUso;
-  protected $HoraDeSubida;
+  protected $fechaantbici;
   protected $id;
   
   public function __construct ($id) {
     $this->id=$id;
+    $this->HoraDeSubida=;
     $this->EnUso=false;
+    $fec=new DateTime('now');
+		$this->fechaantbici = $fec->format('Y\-m\-d\'');
+    
   }
     
   public function Subir {
