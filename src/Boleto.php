baba4a1fa9690d145cafb6de2f975a7b7b4a08eb
candelacorradin <32111070+candelacorradin@users.noreply.github.com>
2017-10-29 23:21:29 -0300

    Update Boleto.php

--- src/Boleto.php
+++ src/Boleto.php
@@ -9,12 +9,16 @@ class Boleto{
 	protected $saldoAcumulado;
 	protected $linea;
 	protected $id_tarj;
+	protected $fechatras;
+	protected $diasemana;
 	public function __construct(Tarjeta $tar, $tipoboleto, $linea, $saldoac = 0){
 		$this->fecha = date('d-m-Y');
 		$this->tar->tipoboleto= $t;
 		$this->hora = date('H:i:s');
 		$this->tar->saldoAcumulado = $saldoac;
 		$this->linea = $linea;
+		$this->fechatras = new DateTime ("now");
+		$this->diasemana = date('w');
 	}
 
 	public getBoleto(){
@@ -31,8 +35,8 @@ class Boleto{
 			$this->tar->saldo = $p;
 			$this->tar->saldoAcumulado = 0;
 		}
-		$this->tar->fechaanterior=$this->fecha;
-		$this->tar->horaanterior=$this->hora;
+		$this->tar->fechaanterior=$this->fechatras;
+		$this->tar->diaanterior=$this->diasemana;
 	}
 	public function Medio(){
 		$p  = $this->tar->saldo - $this->tar->saldoAcumulado - 4.35;
@@ -43,8 +47,8 @@ class Boleto{
 		else{
 			$this->tar->saldo = $p;
 			$this->tar->saldoAcumulado = 0;
-			$this->tar->fechaanterior=$this->fecha;
-			$this->tar->horaanterior=$this->hora;
+			$this->tar->fechaanterior=$this->fechatras;
+			$this->tar->diaanterior=$this->diasemana;
 		}
 	}
 	
@@ -57,8 +61,8 @@ class Boleto{
 	public function ViajePlusNormal(){
 		if($this->tar->saldoAcumulado < (9.70*2)){
 			$this->tar->saldoAcumulado= $this->tar->saldoAcumulado + 9.70;
-			$this->tar->fechaanterior=$this->fecha;
-			$this->tar->horaanterior=$this->hora;
+			$this->tar->fechaanterior=$this->fechatras;
+			$this->tar->diaanterior=$this->diasemana;
 			}
 		else {
 			return "Ya han sido utilizados los dos (2) viajes plus. Recargue su tarjeta.";
