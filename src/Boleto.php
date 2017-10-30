c62f5373c8815d8a5418f029ac7d34f795faba86
candelacorradin <32111070+candelacorradin@users.noreply.github.com>
2017-10-30 17:18:56 -0300

    migra funciones a tarjeta
    queda sólo impresión de boleto

--- src/Boleto.php
+++ src/Boleto.php
@@ -9,8 +9,6 @@ class Boleto{
 	//protected $saldoAcumulado;
 	protected $linea;
 	protected $id_tarj;
-	protected $fechatras;
-	protected $diasemana;
 	protected Tarjeta $tar;
 	protected Colectivo $cole;
 	public function __construct(Tarjeta $tar, 
@@ -24,71 +22,10 @@ class Boleto{
 		//$this->tar->tipoboleto= $t;
 		$this->hora = date('H:i:s');
 		//$this->tar->saldoAcumulado = $saldoac;
-		$this->fechatras = new DateTime ("now");
-		$this->diasemana = date('w');
 	}
 
 	public getBoleto(){
-		print "FECHA: ". $this->fecha . "\nTIPO: ". $this->tar->tipoboleto. "\nLINEA DE COLECTIVO: ". $this->cole->linea . "\nSALDO: ". $this->tar->saldo . "\nID: ". $this->tar->id_tarj;
+		print "FECHA: ". $this->fecha . "\nTIPO: ". $this->tar->tipo. "\nLINEA DE COLECTIVO: ". $this->cole->linea . "\nSALDO: ". $this->tar->saldo . "\nID: ". $this->tar->id_tarj;
 
-	}
-	
-	public function Normal(){
-		$p  = $this->tar->saldo - $this->tar->saldoAcumulado - 9.70;
-		if($p<0){
-			$this->ViajePlus();
-		}
-		else{
-			$this->tar->saldo = $p;
-			$this->tar->saldoAcumulado = 0;
-		}
-		$this->tar->fechaanterior=$this->fechatras;
-		$this->tar->diaanterior=$this->diasemana;
-	}
-	public function Medio(){
-		$p  = $this->tar->saldo - $this->tar->saldoAcumulado - 4.35;
-		if( $p<0 ){
-			echo "No tiene saldo suficiente para pagar medio boleto. Se realizará un viaje plus";
-			$this->ViajePlus();
-		}
-		else{
-			$this->tar->saldo = $p;
-			$this->tar->saldoAcumulado = 0;
-			$this->tar->fechaanterior=$this->fechatras;
-			$this->tar->diaanterior=$this->diasemana;
-		}
-	}
-	
-	public function Trasbordo () {
-		if (this->tipoboleto == "Medio"){
-			$p  = $this->tar->saldo - $this->tar->saldoAcumulado - 1.60;
-		}
-		else {
-			$p  = $this->tar->saldo - $this->tar->saldoAcumulado - 3.20;
-		}
-		if( $p<0 ) {
-			echo "No tiene saldo suficiente para pagar trasbordo. Se realizará un viaje plus";
-			$this->ViajePlus();
-		}
-		else{
-			$this->tar->saldo = $p;
-			$this->tar->saldoAcumulado = 0;
-		}
-	}
-	
-	public function viajeBici(){
-		
-	}
-		
-	}
-	public function ViajePlus(){
-		if($this->tar->saldoAcumulado < (9.70*2)){
-			$this->tar->saldoAcumulado= $this->tar->saldoAcumulado + 9.70;
-			$this->tar->fechaanterior=$this->fechatras;
-			$this->tar->diaanterior=$this->diasemana;
-			}
-		else {
-			return "Ya han sido utilizados los dos (2) viajes plus. Recargue su tarjeta.";
-		}
 	}
 }
