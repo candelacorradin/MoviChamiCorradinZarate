e43331930a0506d84318c4bb09a85537bd18973b
sofiachami <sofiachami99@gmail.com>
2017-10-29 22:45:36 -0300

    test subir

--- tests/BicicletaTest.php
+++ tests/BicicletaTest.php
@@ -2,7 +2,15 @@
 namespace TpFinal;
 use PHPUnit\Framework\TestCase;
 class BicicletaTest extends TestCase {
-    public function testgetId(){
-     $bici= new Bicicleta(1);
-     $this->assertEquals($bici->getId(),1);
-    }}
+    public function testGetid(){
+        $bici= new Bicicleta(1);
+        $this->assertEquals($bici->getId(),1);
+    }
+    public function testSubir(){
+        $bici= new Bicicleta(1);
+        $bici->Subir();
+        $this->assertTrue($bici->EnUso);
+    }
+
+
+}
