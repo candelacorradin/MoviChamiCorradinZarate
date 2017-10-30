1f78b20ecc9ac6e09ffd033f8d9a4cd09213afb3
Aldana Zarate <31429603+alduzarate@users.noreply.github.com>
2017-10-30 17:48:50 -0300

    borrado luego de la mudanza

--- src/Boleto.php
+++ src/Boleto.php
@@ -29,56 +29,5 @@ class Boleto{
 		print "FECHA: ". $this->fecha . "\nTIPO: ". $this->tar->tipo. "\nLINEA DE COLECTIVO: ". $this->cole->linea . "\nSALDO: ". $this->tar->saldo . "\nID: ". $this->tar->id_tarj;
 
 	}
-	public function Normal(){
-		$p  = $this->saldo - $this->saldoAcumulado - 9.70;
-		if($p<0){
-			$this->ViajePlusNormal();
-		}
-		else{
-			$this->saldo = $p;
-			$this->saldoAcumulado = 0;
-		}
-		$this->fechaanterior=$this->fecha;
-		$this->horaanterior=$this->hora;
-	}
-	public function Medio(){
-		$p  = $this->saldo - $this->saldoAcumulado - 4.35;
-		if( $p<0 ){
-			echo "No tiene saldo suficiente para pagar medioboleto. Se realizará un viaje plus";
-			$this->ViajePlusNormal();
-		}
-		else{
-			$this->saldo = $p;
-			$this->saldoAcumulado = 0;
-			$this->fechaanterior=$this->fecha;
-			$this->horaanterior=$this->hora;
-		}
-	}
-	
-	public function Trasbordo () {
-		
-	}
-	public function viajeBici(){
-		if($fechaantbici==" "|| ($fecha->diff($fechaantbici))->d != 0){
-			$this->saldo = $this->saldo - 12.45;
-			$this->fechaanterior=$this->fecha;
-			$this->horaanterior=$this->hora;
-		}
-		$this->getBoleto();
-	
-		}
-		
-		
-	}
-	public function ViajePlusNormal(){
-		if($this->saldoAcumulado < (9.70*2)){
-			$this->saldoAcumulado= $this->saldoAcumulado + 9.70;
-			$this->fechaanterior=$this->fecha;
-			$this->horaanterior=$this->hora;
-			}
-		else {
-			return "Ya han sido utilizados los dos (2) viajes plus. Recargue su tarjeta.";
-		}
-	}
 
 }
