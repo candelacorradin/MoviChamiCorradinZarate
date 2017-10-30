5570b783f2a35aa6dfab73d27204c01f2927ea8e
sofiachami <sofiachami99@gmail.com>
2017-10-29 22:41:12 -0300

    get subir test

--- tests/BicicletaTest.php
+++ tests/BicicletaTest.php
@@ -1,1 +1,8 @@
-
+<?php
+namespace TpFinal;
+use PHPUnit\Framework\TestCase;
+class BicicletaTest extends TestCase {
+    public function testSubir(){
+     $bici= new Bicicleta(1);
+     $this->assertEquals($bici->getId(),1);
+    }
