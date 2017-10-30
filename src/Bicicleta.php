78b8051347e66cf608eb44036399bf2eccb1a662
sofiachami <sofiachami99@gmail.com>
2017-10-30 17:26:03 -0300

    cambio de tipo de variables

--- src/Bicicleta.php
+++ src/Bicicleta.php
@@ -2,9 +2,9 @@
 namespace TpFinal;
 use \Datetime;
 class Bicicleta {
-  protected $EnUso;
-  protected $fechaantbici;
-  protected $id;
+  public $EnUso;
+  public $fechaantbici;
+  public $id;
   
   public function __construct ($id) {
     $this->id=$id;
