629d9f6dec6d97d1ab25799f1ff5262393c5b923
Aldana Zarate <31429603+alduzarate@users.noreply.github.com>
2017-10-30 21:55:49 -0300

    test para el constructor y para getBoleto

--- tests/BoletoTest.php
+++ tests/BoletoTest.php
@@ -1,1 +1,22 @@
-
+<?php
+namespace TpFinal;
+use PHPUnit\Framework\TestCase;
+class BoletoTest extends TestCase {
+  
+  public function testConstructor(){
+    $tarjeta = new Tarjeta(1234,"Normal");
+    $cole = new Colectivo("122 verde","Semtur");
+    $b = new Boleto($tarjeta,$cole);
+    $this->assertEquals($b->tar,$tarjeta);
+    $this->assertEquals($b->cole,$cole);
+    $this->assertEquals($b->fecha,date('d-m-Y'));
+    $this->assertEquals($b->hora,date('H:i:s'));
+  }
+  public function testgetBoleto(){
+      $tarjeta = new Tarjeta(1234,"Normal");
+      $cole = new Colectivo("122 verde","Semtur");
+      $b = new Boleto($tarjeta,$cole);
+      $this->assertEquals($b->getBoleto(), "FECHA: " . $this->fecha . "\nTIPO: Normal\nLINEA DE COLECTIVO: 122 verde\nSALDO: 0\nID: 1234");
+  }
+  
+}
