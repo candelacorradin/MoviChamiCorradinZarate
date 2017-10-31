81ad18a49f090817251045438638f0171c65e3a6
Aldana Zarate <31429603+alduzarate@users.noreply.github.com>
2017-10-31 16:58:32 -0300

    saco fecha anterior
    QUEDAN EN TARJETA

--- src/Bicicleta.php
+++ src/Bicicleta.php
@@ -3,15 +3,12 @@ namespace TpFinal;
 use \Datetime;
 class Bicicleta {
   public $EnUso;
-  public $fechaantbici;
   public $id;
   
   public function __construct ($id) {
     $this->id=$id;
     $this->EnUso=false;
-    $fec=new DateTime('now');
-    $this->fechaantbici = $fec->format('Y\-m\-d\'');
-    
+      
   }
   
   public function getId(){
