253ac7b728d04d8e2134e04b444e6201e26c0af6
Aldana Zarate <31429603+alduzarate@users.noreply.github.com>
2017-11-02 19:13:43 -0300

    cambios para el estándar de codificación
    cambios para el estándar
    https://make.wordpress.org/core/handbook/best-practices/coding-standards/php/

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -9,119 +9,116 @@ class EstacionTest extends TestCase {
     /**
      * Comprueba que el saldo de una tarjeta nueva sea cero.
      */
-    public function testIdNormal(){
-     $tarjeta = new Tarjeta(1234,"Normal");
-     $this->assertEquals($tarjeta->getId(),1234);
-     $this->assertEquals($tarjeta->getTipo(),"Normal");
+    public function test_id_normal(){
+     $tarjeta = new Tarjeta( 1234, "Normal" );
+     $this->assertEquals( $tarjeta->get_id(), 1234 );
+     $this->assertEquals( $tarjeta->get_tipo(), "Normal" );
         
     }
-    public function testSaldoCeroMedio() {
-        $tarjeta = new Tarjeta(1234,"Medio");
-        $this->assertEquals($tarjeta->saldo(), 0);
-        $this->assertEquals($tarjeta->getTipo(),"Medio");
+    public function test_saldo_cero_medio() {
+        $tarjeta = new Tarjeta( 1234, "Medio" );
+        $this->assertEquals( $tarjeta->saldo(), 0 );
+        $this->assertEquals( $tarjeta->get_tipo(), "Medio" );
     }
-    public function testSaldoCincuenta(){
+    public function test_saldo_cincuenta(){
         $tarjeta = new Tarjeta(1234,"Normal");
-        $tarjeta->cargar(50);
-        $this->assertEquals($tarjeta->getSaldo(),50);
+        $tarjeta->cargar( 50 );
+        $this->assertEquals( $tarjeta->get_saldo(), 50 );
     }
-    public function testSaldoTresTresDos(){
-        $tarjeta = new Tarjeta(1234,"Normal");
-        $tarjeta->cargar(332);
-        $this->assertEquals($tarjeta->getSaldo(),388);
+    public function test_saldo_tres_tres_dos(){
+        $tarjeta = new Tarjeta( 1234, "Normal" );
+        $tarjeta->cargar( 332 );
+        $this->assertEquals( $tarjeta->get_saldo(), 388 );
     }
-    public function testSaldoSeisDosCuatro(){
-        $tarjeta = new Tarjeta(1234,"Normal");
-        $tarjeta->cargar(624);
-        $this->assertEquals($tarjeta->getSaldo(),776);
+    public function test_saldo_seis_dos_cuatro(){
+        $tarjeta = new Tarjeta( 1234, "Normal" );
+        $tarjeta->cargar( 624 );
+        $this->assertEquals( $tarjeta->get_saldo(), 776 );
     }
-    public function testDosViajes(){
-            $tarjeta = new Tarjeta(1234,"Normal");
-            $colectivo = new Colectivo ("120", "Semtur");
-            $tarjeta->cargar(50);
-            $tarjeta->Viaje($colectivo);
-            $tarjeta->Viaje($colectivo);
-            $this->assertEquals($tarjeta->getSaldo(),30.6);    
+    public function test_dos_viajes(){
+            $tarjeta = new Tarjeta( 1234, "Normal" );
+            $colectivo = new Colectivo ( "120", "Semtur" );
+            $tarjeta->cargar( 50 );
+            $tarjeta->viaje( $colectivo );
+            $tarjeta->viaje( $colectivo );
+            $this->assertEquals( $tarjeta->get_saldo(), 30.6 );    
     }
-    public function testviajeTrasAmigue(){
-        $tarjeta = new Tarjeta(1234, "Normal");
-        $tarjeta->cargar(40);
-        $colectivo = new Colectivo ("120", "Semtur");
-        $colectivo2 = new Colectivo ("121", "Semtur");
-        $tarjeta->Viaje($colectivo);
+    public function test_viaje_tras_amigue(){
+        $tarjeta = new Tarjeta( 1234, "Normal" );
+        $tarjeta->cargar( 40 );
+        $colectivo = new Colectivo ( "120", "Semtur" );
+        $colectivo2 = new Colectivo ( "121", "Semtur" );
+        $tarjeta->viaje( $colectivo );
         //hizo un viaje normal, ahora el saldo tiene que ser 30.3
-        $this->assertEquals($tarjeta->getSaldo(),30.3);
-        $fecha = new \DateTime("now");
-        $tarjeta->fechaanterior=$fecha->sub(new \DateInterval('PT0H1800S'));
-        $tarjeta->Viaje($colectivo2);
-        $this->assertEquals($tarjeta->getSaldo(),27.1);
+        $this->assertEquals( $tarjeta->get_saldo(), 30.3 );
+        $fecha = new \DateTime( "now" );
+        $tarjeta->fechaanterior = $fecha->sub( new \DateInterval( 'PT0H1800S' ) );
+        $tarjeta->viaje( $colectivo2 );
+        $this->assertEquals( $tarjeta->get_saldo(), 27.1 );
         //hizo trasbordo
-        $tarjeta->Viaje($colectivo2); //le paga al otre
-        $this->assertEquals($tarjeta->getSaldo(),17.4);
+        $tarjeta->viaje( $colectivo2 ); //le paga al otre
+        $this->assertEquals( $tarjeta->get_saldo(), 17.4 );
     }
     
-    public function testTrasbordo(){
-        $tarjeta = new Tarjeta(1234, "Normal");
-        $tarjeta->cargar(40);
-        $colectivo = new Colectivo ("120", "Semtur");
-        $colectivo2 = new Colectivo ("121", "Semtur");
-        $tarjeta->Viaje($colectivo);
+    public function test_trasbordo(){
+        $tarjeta = new Tarjeta( 1234, "Normal" );
+        $tarjeta->cargar( 40 );
+        $colectivo = new Colectivo ( "120", "Semtur" );
+        $colectivo2 = new Colectivo ( "121", "Semtur" );
+        $tarjeta->viaje( $colectivo );
         //hizo un viaje normal, ahora el saldo tiene que ser 30.3
-        $this->assertEquals($tarjeta->getSaldo(),30.3);
-        $fecha = new \DateTime("now");
-        $tarjeta->fechaanterior=$fecha->sub(new \DateInterval('PT0H1800S'));;
-        $tarjeta->Viaje($colectivo2);
-        $this->assertEquals($tarjeta->getSaldo(),27.1);
+        $this->assertEquals( $tarjeta->get_saldo(), 30.3 );
+        $fecha = new \DateTime( "now" );
+        $tarjeta->fechaanterior = $fecha->sub( new \DateInterval( 'PT0H1800S' ) );
+        $tarjeta->viaje( $colectivo2 );
+        $this->assertEquals( $tarjeta->get_saldo(), 27.1 );
         //hizo trasbordo
-        
     }
-    public function testViajeColectivoNormal(){
-        $tarjeta = new Tarjeta(1234, "Normal");
-        $tarjeta->cargar(40);
-        
-        $colectivo = new Colectivo ("120", "Semtur");
-        $tarjeta->Viaje($colectivo);
-        $this->assertEquals($tarjeta->getSaldo(), 30.3);
+    public function test_viaje_colectivo_normal(){
+        $tarjeta = new Tarjeta( 1234, "Normal" );
+        $tarjeta->cargar( 40 );
+        $colectivo = new Colectivo ( "120", "Semtur" );
+        $tarjeta->viaje( $colectivo );
+        $this->assertEquals( $tarjeta->get_saldo(), 30.3 );
     }
-    public function testViajeColectivoMedio(){
-        $tarjeta = new Tarjeta(1234, "Medio");
-        $tarjeta->cargar(40);
-        
-        $colectivo = new Colectivo ("120", "Semtur");
-        $tarjeta->Viaje($colectivo);
-        $this->assertEquals($tarjeta->getSaldo(), 40-4.35);
+    public function test_viaje_colectivo_medio(){
+        $tarjeta = new Tarjeta( 1234, "Medio" );
+        $tarjeta->cargar( 40 );
+        $colectivo = new Colectivo ( "120", "Semtur" );
+        $tarjeta->viaje( $colectivo );
+        $this->assertEquals( $tarjeta->get_saldo(), 40-4.35 );
     }
-    public function testViajeColectivoMedioPlus(){
-        $tarjeta = new Tarjeta(1234, "Medio");
-        $colectivo = new Colectivo ("120", "Semtur");
-        $tarjeta->Viaje($colectivo);
-        $this->assertEquals($tarjeta->saldoAcumulado, 9.70);
+    public function test_viaje_colectivo_medio_plus(){
+        $tarjeta = new Tarjeta( 1234, "Medio" );
+        $colectivo = new Colectivo ( "120", "Semtur" );
+        $tarjeta->viaje( $colectivo );
+        $this->assertEquals( $tarjeta->saldo_acumulado, 9.70 );
     }
-    public function testViajeColectivoNormalPlus(){
-        $tarjeta = new Tarjeta(1234, "Normal");
-        $colectivo = new Colectivo ("120", "Semtur");
-        $tarjeta->Viaje($colectivo);
-        $this->assertEquals($tarjeta->saldoAcumulado, 9.70);
+    public function test_viaje_colectivo_normal_plus(){
+        $tarjeta = new Tarjeta( 1234, "Normal" );
+        $colectivo = new Colectivo ( "120", "Semtur" );
+        $tarjeta->viaje( $colectivo );
+        $this->assertEquals( $tarjeta->saldo_acumulado, 9.70 );
     }
-    public function testViajeBici() {
-        $tarjeta = new Tarjeta(1234, "Normal");
-        $tarjeta->cargar(40);
-        $bici = new Bicicleta(456);
-        $tarjeta->Viaje($bici);
-        $this->assertEquals($tarjeta->getSaldo(),27.55);
+    public function test_viaje_bici() {
+        $tarjeta = new Tarjeta( 1234, "Normal" );
+        $tarjeta->cargar( 40 );
+        $bici = new Bicicleta( 456 );
+        $tarjeta->viaje( $bici );
+        $this->assertEquals( $tarjeta->get_saldo(), 27.55 );
     }
-    public function testAlquilarVariasVeces(){
-        $tarjeta = new Tarjeta(1234, "Normal");
-        $tarjeta->cargar(40);
-        $bici = new Bicicleta (120);
-        $bici2 = new Bicicleta (121);
-        $tarjeta->Viaje($bici);
+    public function test_alquilar_varias_veces(){
+        $tarjeta = new Tarjeta( 1234, "Normal" );
+        $tarjeta->cargar( 40 );
+        $bici = new Bicicleta ( 120 );
+        $bici2 = new Bicicleta ( 121 );
+        $tarjeta->viaje( $bici );
         //hizo un viaje normal, ahora el saldo tiene que ser 27.55
-        $this->assertEquals($tarjeta->getSaldo(),27.55);
-        $fecha = new \DateTime("now");
-        $tarjeta->fechaanterior=$fecha->sub(new \DateInterval('PT0H1800S'));;
-        $tarjeta->Viaje($bici2);
+        $this->assertEquals( $tarjeta->get_saldo(), 27.55 );
+        $fecha = new \DateTime( "now" );
+        $tarjeta->fechaanterior = $fecha->sub( new \DateInterval( 'PT0H1800S' ) );
+        $tarjeta->viaje( $bici2 );
         //el saldo no tiene que cambiar ya que es el mismo dia
-        $this->assertEquals($tarjeta->getSaldo(),27.55);
+        $this->assertEquals( $tarjeta->get_saldo(), 27.55 );
     }
 }
