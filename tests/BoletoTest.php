993e3d9f6e784afe056acfd7365d6d1058cc7d8d
Aldana Zarate <31429603+alduzarate@users.noreply.github.com>
2017-11-02 19:16:20 -0300

    cambios para codificación
    estándar https://make.wordpress.org/core/handbook/best-practices/coding-standards/php/

--- tests/BoletoTest.php
+++ tests/BoletoTest.php
@@ -3,20 +3,20 @@ namespace TpFinal;
 use PHPUnit\Framework\TestCase;
 class BoletoTest extends TestCase {
   
-  public function testConstructor(){
-    $tarjeta = new Tarjeta(1234,"Normal");
-    $cole = new Colectivo("122 verde","Semtur");
-    $b = new Boleto($tarjeta,$cole);
-    $this->assertEquals($b->tar,$tarjeta);
-    $this->assertEquals($b->transporte,$cole);
-    $this->assertEquals($b->fecha,date('d-m-Y'));
-    $this->assertEquals($b->hora,date('H:i:s'));
+  public function test_constructor(){
+    $tarjeta = new Tarjeta( 1234, "Normal" );
+    $cole = new Colectivo( "122 verde", "Semtur" );
+    $b = new Boleto( $tarjeta, $cole );
+    $this->assertEquals( $b->tar, $tarjeta );
+    $this->assertEquals( $b->transporte, $cole );
+    $this->assertEquals( $b->fecha, date( 'd-m-Y' ) );
+    $this->assertEquals( $b->hora, date( 'H:i:s' ) );
   }
-  public function testgetBoleto(){
-      $tarjeta = new Tarjeta(1234,"Normal");
-      $cole = new Colectivo("122 verde","Semtur");
-      $b = new Boleto($tarjeta,$cole);
-      $this->assertEquals($b->getBoleto(), "FECHA: " . $b->fecha . "\nTIPO: Normal\nLINEA DE COLECTIVO: 122 verde\nSALDO: 0\nID: 1234");
+  public function test_get_boleto(){
+      $tarjeta = new Tarjeta( 1234, "Normal" );
+      $cole = new Colectivo( "122 verde", "Semtur" );
+      $b = new Boleto( $tarjeta, $cole );
+      $this->assertEquals( $b->get_boleto(), "FECHA: " . $b->fecha . "\nTIPO: Normal\nLINEA DE COLECTIVO: 122 verde\nSALDO: 0\nID: 1234" );
   }
   
 }
