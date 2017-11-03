ba5700d691c96586d77168dabe61c6bb67f50c0e
Aldana Zarate <31429603+alduzarate@users.noreply.github.com>
2017-11-03 01:44:28 -0300

    vuelta a estandarizar luego de descubrir el verdadero error

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -3,15 +3,15 @@ namespace TpFinal;
 class Colectivo {
 public $linea;    //lo pongo pubic nomas porque sino me falla el test
 protected $empresa;
- public function __construct ($linea,$empresa){
+ public function __construct ( $linea, $empresa ){
     $this->linea = $linea;
     $this->empresa = $empresa;
   }
   
-  public function getLinea(){
-   return $this->linea;
+  public function get_linea(){
+     return $this->linea;
   }
-  public function getEmpresa(){
+  public function get_empresa(){
      return $this->empresa;
   }
 }
