8399bcca2dde2606244df1b3e99bf629642217e5
Aldana Zarate <31429603+alduzarate@users.noreply.github.com>
2017-11-03 01:05:25 -0300

    VER PQ SE ROMPE TRASBORDO

--- tests/BicicletaTest.php
+++ tests/BicicletaTest.php
@@ -2,18 +2,18 @@
 namespace TpFinal;
 use PHPUnit\Framework\TestCase;
 class BicicletaTest extends TestCase {
-    public function test_get_id(){
-        $bici = new Bicicleta( 1 );
-        $this->assertEquals( $bici->get_id(), 1 );
+    public function testgetId(){
+        $bici= new Bicicleta(1);
+        $this->assertEquals($bici->getId(),1);
     }
-    public function test_subir(){
-        $bici = new Bicicleta( 1 );
-        $bici->subir();
-        $this->assertTrue( $bici->en_uso );
+    public function testSubir(){
+        $bici= new Bicicleta(1);
+        $bici->Subir();
+        $this->assertTrue($bici->EnUso);
     }
-    public function test_bajar(){
-        $bici = new Bicicleta( 2 );
-        $bici->bajar();
-        $this->assertFalse( $bici->en_uso );
+    public function testBajar(){
+        $bici= new Bicicleta(2);
+        $bici->Bajar();
+        $this->assertFalse($bici->EnUso);
     }
 }
