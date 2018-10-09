5ed491f6737ffdd487f9072288c41dac81b409c6
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2018-10-09 16:05:23 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/Bicicleta.php
+++ src/Bicicleta.php
@@ -1,23 +1,23 @@
 <?php
 namespace TpFinal;
 class Bicicleta {
-  public $en_uso;
-  public $id;
+    public $en_uso;
+    public $id;
   
-   public function __construct ( $id ) {
+    public function __construct ( $id ) {
     $this->id = $id;
     $this->en_uso = false;
-   }
+    }
   
-  public function get_id(){
+    public function get_id(){
     return $this->id;
-  }
+    }
     
-  public function subir(){
+    public function subir(){
     $this->en_uso = true;
-  }
+    }
   
-  public function bajar(){
+    public function bajar(){
     $this->en_uso = false;
-  }
+    }
 }
